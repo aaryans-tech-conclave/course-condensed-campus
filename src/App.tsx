0676f8f91b4ed69f551ad1bc6fd34bb63ce22ecc
Kumar-ti <kumar.aaryan@trilogy.com>
2025-07-15 13:17:40 +0530

    edited routing

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/course-condensed-campus">
         <div className="min-h-screen bg-background">
           <Header />
           <Routes>
